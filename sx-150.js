2566f746e2eb04149c6691cc1e101737c79362aa
Mark Durrant <mark.j.durrant.gmail.com>
2015-09-03 16:17:55 +0100

    add simple sequ page

--- sx-150.js
+++ sx-150.js
@@ -4,33 +4,41 @@ var board = new five.Board();
 board.on("ready", function() {
   var pitch = new five.Led(10);
   var relay = new five.Relay(9)
-      relay.close();
+      // relay.close();
 
-  var delay = 500;
+  var delay = 250;
   var bar = 16;
   var noteIndex = 0;
 
-  var pA = [0, 100, 0, 100,
-            0, 100, 0, 100,
-            0, 100, 0, 100,
-            0, 100, 0, 100 ];
+  var pA = [0, 1, 2, 3, 4, 5, 6, 7,
+            0, 1, 2, 3, 4, 5, 6, 7];
 
-  var pB = [0, 100, 100, 100,
-            0, 100, 100, 100,
-            0, 100, 100, 100,
-            0, 100, 100, 100 ];
+  var pB = [0, 2, 2, 6, 0, 2, 2, 6,
+            0, 4, 3, 7, 0, 2, 2, 6 ];
+
+  var pC = [4, 3, 2, 5, 4, 3, 3, 3,
+            4, 3, 2, 5, 1, 1, 2, 5 ];
+
+  var pD = [1, 2, 5, 3, 1, 0, 0, 3,
+            1, 2, 5, 3, 1, 2, 5, 3 ];
+
+  var pE = [6, 2, 6, 2, 6, 2, 0, 2,
+            5, 1, 5, 1, 5, 1, 0, 1 ];
+
+  var pF = [0, 2, 0, 2, 0, 2, 0, 2,
+            0, 0, 0, 2, 0, 4, 0, 4 ];
 
   var currentPatern = pA;
 
   (function playNote () {
      setTimeout(function () {
         if(currentPatern[noteIndex] === 0) {
-          relay.close();
+          // relay.close();
+          pitch.brightness(currentPatern[noteIndex] * 255 / 7);
         } else {
-          relay.open();
+          // relay.open();
+          pitch.brightness(currentPatern[noteIndex] * 255 / 7);
         }
-
-        pitch.brightness(currentPatern[noteIndex]);
 
         noteIndex++;
         if (noteIndex >= bar) {
@@ -46,13 +54,12 @@ board.on("ready", function() {
       delay = setDelay;
       return delay;
     },
-    pA: function() {
-      currentPatern = pA;
-      return currentPatern;
-    },
-    pB: function() {
-      currentPatern = pB;
-      return currentPatern;
-    }
+    pA: function() { currentPatern = pA; return currentPatern; },
+    pB: function() { currentPatern = pB; return currentPatern; },
+    pC: function() { currentPatern = pC; return currentPatern; },
+    pD: function() { currentPatern = pD; return currentPatern; },
+    pE: function() { currentPatern = pE; return currentPatern; },
+    pF: function() { currentPatern = pF; return currentPatern; },
+    pattern: function(newPattern) {currentPatern = newPattern; return currentPatern; }
   });
 });
